574640cb89d79900d2a390e035d9681961291f65
pradeepahirwarjul92 <pradeepahirwarjul92@gmail.com>
2025-09-04 13:22:56 +0530

    new print statement added

--- test-git/src/main/java/com/heg/model/TestClass.java
+++ test-git/src/main/java/com/heg/model/TestClass.java
@@ -3,6 +3,7 @@ package com.heg.model;
 public class TestClass {
 public void add(int a, int b){
 	System.out.println(a+b);
+	System.out.println("Hii");
 }
 	
 	public static void main(String[] args) {
