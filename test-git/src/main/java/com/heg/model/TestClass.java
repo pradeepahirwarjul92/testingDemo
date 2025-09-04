a6a5d0e4bb6070cf1fca737d4dbd97134c776e49
pradeepahirwarjul92 <pradeepahirwarjul92@gmail.com>
2025-09-04 13:24:51 +0530

    multiplication method added

--- test-git/src/main/java/com/heg/model/TestClass.java
+++ test-git/src/main/java/com/heg/model/TestClass.java
@@ -6,6 +6,10 @@ public void add(int a, int b){
 	System.out.println("Hii");
 }
 	
+public void multiplicationMethod(int a, int b){
+	System.out.println(a*b);
+	System.out.println("Hii");
+}
 	public static void main(String[] args) {
 		System.out.println("TestClass.main()");
 		
